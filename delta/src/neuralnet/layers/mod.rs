fa19cec541dcfa987ea54ba0bd7383bff6aa6463
Marcus Cveticanin <k01522@norsk-tipping.no>
2024-12-06 14:08:31 +0100

    Re-exporting Conv2D

--- delta/src/neuralnet/layers/mod.rs
+++ delta/src/neuralnet/layers/mod.rs
@@ -30,7 +30,9 @@
 pub mod dense;
 pub mod flatten;
 pub mod max_pooling_2d;
+pub mod conv_2d;
 
 pub use dense::Dense;
 pub use flatten::Flatten;
 pub use max_pooling_2d::MaxPooling2D;
+pub use conv_2d::Conv2D;
