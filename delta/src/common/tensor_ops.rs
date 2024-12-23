31e47e2051e9fcd0b883b328ad96d59598726215
mjovanc <mjovanc@icloud.com>
2024-12-23 22:05:47 +0100

    Cargo fmt fix

--- delta/src/common/tensor_ops.rs
+++ delta/src/common/tensor_ops.rs
@@ -8,9 +8,7 @@ use rand_distr::{Distribution, Normal};
 
 use crate::devices::Device;
 #[cfg(all(target_os = "macos", feature = "metal"))]
-use crate::devices::osx_metal::{
-    tensor_add_metal, tensor_divide_metal, tensor_subtract_metal,
-};
+use crate::devices::osx_metal::{tensor_add_metal, tensor_divide_metal, tensor_subtract_metal};
 
 /// A struct representing a tensor.
 #[derive(Debug, Clone)]
