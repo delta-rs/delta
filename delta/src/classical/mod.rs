ac6fedc5c43c1bb424b5635fec1038f43f2df341
Marcus Cveticanin <k01522@norsk-tipping.no>
2025-01-23 12:59:10 +0100

    Updating stuff in classical module

--- delta/src/classical/mod.rs
+++ delta/src/classical/mod.rs
@@ -3,7 +3,23 @@ pub mod clustering;
 pub mod dimensionality_reduction;
 pub mod regression;
 
+pub use classification::LinearRegression;
+
 use ndarray::{Array1, Array2};
+
+// Define a trait for all Classical models
+pub trait Classical {
+    /// Create a new instance of the model
+    fn new() -> Self
+    where
+        Self: Sized;
+
+    /// Fit the model to the data
+    fn fit(&mut self, x: &Array2<f64>, y: &Array1<f64>, learning_rate: f64, epochs: usize);
+
+    /// Use the model to make predictions
+    fn predict(&self, x: &Array2<f64>) -> Array1<f64>;
+}
 
 /// Calculate Mean Squared Error loss
 pub fn calculate_loss(predictions: &Array1<f64>, actuals: &Array1<f64>) -> f64 {
@@ -16,12 +32,13 @@ pub fn calculate_loss(predictions: &Array1<f64>, actuals: &Array1<f64>) -> f64 {
 pub fn gradient_descent(
     x: &Array2<f64>,
     y: &Array1<f64>,
-    predictions: &Array1<f64>,
     weights: &Array1<f64>,
+    bias: f64,
 ) -> (Array1<f64>, f64) {
+    let predictions = x.dot(weights) + bias;
     let m = x.shape()[0] as f64;
 
-    let grad_weights = x.t().dot(&(predictions - y)) / m;
+    let grad_weights = x.t().dot(&(predictions.clone() - y)) / m;
     let grad_bias = (predictions - y).sum() / m;
 
     (grad_weights, grad_bias)
